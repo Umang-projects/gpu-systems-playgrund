7ac7ee8635a8ef32e1c4284fd419fc54a6a0328b
Umang Singh <umang2001umang@gmail.com>
2025-12-21 22:29:36 +0530

    Add Types_warp_working folder with shuffle examples

---
+++ Warp_level_programming/Warp_and_find_id/Kernels_Intro/Types_warp_working/shuffle_up.cu
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+__global__ void shift_up_example() {
+    int lane=threadIdx.x%32;
+    int my_value=lane;
+    
+    // Get value from 1 position above (previous thread)
+    int from_above=__shfl_up_sync(0xFFFFFFFF,my_value,1);
+    
+    if (lane < 8) {
+        printf("Thread %d: my_value=%d, from_above=%d\n",
+lane,my_value,from_above);
+    }
+}
+
+int main() {
+    shift_up_example<<<1, 32>>>();
+    cudaDeviceSynchronize();
+    return 0;
+}
